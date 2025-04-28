b71ef9c9e2079e53a60347bf0f40b6cb6005135c
zSakai <onesakkai@gmail.com>
2025-04-28 16:57:07 -0300

    Primeiro commit: Estrutura inicial do projeto Angular

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  imports: [],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.scss'
+})
+export class FirstComponentComponent {
+
+}
